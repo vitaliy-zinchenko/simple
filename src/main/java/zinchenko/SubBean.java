835215691421d01258b98a7c7ca526810591cfb4
vitaliy-zinchenko <zinjvi@gmail.com>
2014-07-14 09:39:05 +0300

    c

---
+++ src/main/java/zinchenko/SubBean.java
@@ -0,0 +1,37 @@
+package zinchenko;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by zinchenko on 29.06.14.
+ */
+@Entity
+@Table(name = "sub_bean")
+public class SubBean {
+
+    @Id
+    @Column(name = "sub_bean_id")
+    private Long id;
+
+    @Column(name = "value")
+    private String value;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
